8b88296a769e4bb942e2cdd9cb2f603df8f7454b
slukas111 <sashamati@gmail.com>
2020-03-20 15:29:16 -0700

    fixed comments and white space portion A

--- snake.py
+++ snake.py
@@ -28,23 +28,28 @@ win.addch(food[0], food[1], '*')
 # While Esc key is not pressed
 while key != 27:
     win.border(0)
-    win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
-    win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-    win.timeout(150 - (len(snake)/5 + len(snake)/10)%120)          # Increases the speed of Snake as its length increases
-    
-    prevKey = key                                                  # Previous key pressed
+    # Printing 'Score' and
+    win.addstr(0, 2, 'Score : ' + str(score) + ' ')
+    # 'SNAKE' strings
+    win.addstr(0, 27, ' SNAKE ')
+    # Increases the speed of Snake as its length increases
+    win.timeout(150 - (len(snake)/5 + len(snake)/10) % 120)
+
+    # Previous key pressed
+    prevKey = key
     event = win.getch()
-    key = key if event == -1 else event 
+    key = key if event == -1 else event
 
-
-    if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-        key = -1                                                   # one (Pause/Resume)
+# If SPACE BAR is pressed, wait for another
+    if key == ord(' '):
+        # one (Pause/Resume)
+        key = -1
         while key != ord(' '):
             key = win.getch()
         key = prevKey
         continue
-
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+# If an invalid key is pressed
+    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:
         key = prevKey
 
     # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
@@ -52,7 +57,8 @@ while key != 27:
     snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
 
     # If snake crosses the boundaries, make it enter from the other side
-    if snake[0][0] == 0: snake[0][0] = 18
+    if snake[0][0] == 0: 
+        snake[0][0] = 18
     if snake[0][1] == 0: snake[0][1] = 58
     if snake[0][0] == 19: snake[0][0] = 1
     if snake[0][1] == 59: snake[0][1] = 1
