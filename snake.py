07084b49846af76da0258843f186bc39bb15c330
slukas111 <sashamati@gmail.com>
2020-03-20 15:35:06 -0700

    fixed white space/errors snake crossing comment

--- snake.py
+++ snake.py
@@ -67,7 +67,8 @@ while key != 27:
         snake[0][1] = 1
 
     # Exit if snake crosses the boundaries (Uncomment to enable)
-    #if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
+    # if snake[0][0] == 0 or snake[0][0] == 19 or
+    # snake[0][1] == 0 or snake[0][1] == 59: break
 
     # If snake runs over itself
     if snake[0] in snake[1:]: break
